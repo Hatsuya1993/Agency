3afba36cb41c7d89f7f2cba12b93ee604743f391
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-08-08 08:47:20 +0800

    projects done

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // init Isotope
+    let $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+})
